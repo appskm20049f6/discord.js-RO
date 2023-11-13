38656f35ff4291498c67a799c808e8c386f66c87
Koi tw <appskm20049f6@gmail.com>
2023-11-14 01:57:37 +0800

    選擇等級地圖第一版

--- src/commands/expMap/index.js
+++
@@ -1,9 +0,0 @@
-import {SlashCommandBuilder} from 'discord.js'
-
-export const command = new SlashCommandBuilder()
-  .setName("練等地圖")
-  .setDescription("幾等要去哪裡練，記得輸入自己的等級喔");
-
-export const action = async (ctx) => {
-  ctx.reply("請輸入你的等級喔！數字！");
-};
