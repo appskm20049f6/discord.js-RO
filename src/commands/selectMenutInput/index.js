734b8cc38fc6aa76063216b53a0b9e399f3f30ff
Koi tw <appskm20049f6@gmail.com>
2023-11-14 18:03:09 +0800

    新增了一個採googleSheet當成DB的一個測試用，有成功讓他正常運作了，但是內部的演算法還沒搞

--- src/commands/selectMenutInput/index.js
+++ src/commands/selectMenutInput/index.js
@@ -18,8 +18,27 @@ export const action = async (ctx) => {
   const replayContent = () => {
     if (levelInput < 100) {
       return `你現在的等級是${levelInput}等，推薦練功地點是。https://cdn.discordapp.com/attachments/1173797883481432134/1173797905098870885/image.png`;
-    } else {
-      return `等級：${levelInput}製作中`;
+    }
+    if (levelInput >= 110 && levelInput < 125) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173800066230800517/image.png`;
+    } else if (levelInput >= 125 && levelInput < 150) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173800648093991012/image.png`;
+    } else if (levelInput >= 140 && levelInput < 155) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173801451416469504/image.png`;
+    } else if (levelInput >= 150 && levelInput < 170) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173803851984404580/image.png`;
+    } else if (levelInput >= 160 && levelInput < 170) {
+      return `https://media.discordapp.net/attachments/1173805554074931220/1173831295533580298/image.png`;
+    } else if (levelInput >= 170 && levelInput < 185) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173803925544124467/image.png`;
+    } else if (levelInput >= 180 && levelInput < 195) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173803976467169322/image.png`;
+    } else if (levelInput >= 185 && levelInput < 200) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173804011837726852/image.png`;
+    } else if (levelInput >= 185 && levelInput < 200) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173804156725776384/image.png`;
+    } else if (levelInput >= 190 && levelInput < 200) {
+      return `https://media.discordapp.net/attachments/1173797883481432134/1173804055173275648/image.png`;
     }
   };
 
