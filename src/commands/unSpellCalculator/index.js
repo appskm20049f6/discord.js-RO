3f3b22982b42a2f0e5453213b994e490aec8c5df
Koi tw <appskm20049f6@gmail.com>
2023-11-17 15:16:28 +0800

    新增無詠數值計算機

---
+++ src/commands/unSpellCalculator/index.js
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export const command = new SlashCommandBuilder()
+  .setName("數值無詠計算機")
+  .setDescription("無詠計算機，請勿隨意更動公式");
+
+export const action = async (ctx) => {
+  ctx.reply(
+    "https://docs.google.com/spreadsheets/d/1AA9TqKp7tL_uOQRihMbWJ1mwOUAI-UeTeVWui8BNoKA/edit#gid=1138635395"
+  );
+};
