a7c018f5ec12593a1f1365ca2e9ccfd1badf6f57
Koi tw <appskm20049f6@gmail.com>
2023-11-17 11:41:44 +0800

    在main.js增加斷線後自動重連

--- src/main.js
+++ src/main.js
@@ -21,6 +21,20 @@ appStore.client = client;
 
 loadEvents();
 
+// 在斷線時觸發
+client.on('disconnect', (event) => {
+    console.error(`已斷線，代碼：${event.code}, 原因：${event.reason}`);
+    // 這裡可以執行重連邏輯
+    setTimeout(() => {
+      console.log('嘗試重新連線...');
+      client.login(process.env.TOKEN);
+    }, 5000); // 5 秒後重新連線
+  });
+  
+  // 在錯誤時觸發
+  client.on('error', (error) => {
+    console.error('發生錯誤：', error);
+  });
 
 
 // Log in to Discord with your client's token
